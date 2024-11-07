9c7d8d9835689118116d6ccb878c80ed61a8d121
gtopchiyski <gtopchiyski@tu-sofia.bg>
2024-11-08 00:14:44 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let display = document.getElementById("display");
 
+function clearDisplay() {
+    display.value = "";
+}
+
 function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
