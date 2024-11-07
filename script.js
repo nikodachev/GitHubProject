e8273ccd14a168ff851d4b043cc827a0cb97d51f
gtopchiyski <gtopchiyski@tu-sofia.bg>
2024-11-07 23:58:41 +0200

    Main Files

---
+++ script.js
@@ -0,0 +1,31 @@
+let display = document.getElementById("display");
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        display.value = display.value.slice(0, -1);
+    }
+    display.value += operator;
+}
+
+function appendDecimal() {
+    if (!display.value.includes(".")) {
+        display.value += ".";
+    }
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
